7ab706ef9434cc8b0fc45c98ae8f8f797fcb68fc
Mohammad_Zakaria_Yunus <mohammadzakariaabdullahyunus99@gmail.com>
2023-11-17 18:47:01 +0600

    first project start

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const ac = 40;
+
+  res.send(ac);
+});
+
+export default app;
